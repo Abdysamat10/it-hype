c20ac69ac85fae897deb4c27b243c91aa5858cc0
Abdysamat10 <64656988+Abdysamat10@users.noreply.github.com>
2021-07-29 22:36:53 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ window.addEventListener("scroll", function(){
 
 // #    #     #     #    H  E  A  D  E  R    #    #     #     #
 
+// #    #     #     #    M  A  P    #    #     #     #
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+
+// #    #     #     #    M  A  P    #    #     #     #
+
 // #    #     #     #    L  E  A  R  N    #    #     #     #
 const btn = document.querySelector('.learn__body');
 btn.onmousemove = function(e){
@@ -137,5 +157,73 @@ $(document).ready(function(){
     })
 })
 // #    #     #     #   B  U  R  G  E  R     #    #     #     #
+
+// #    #     #     #    F  O  R  M     #    #     #     #
+
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form__body');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e){
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        if (error === 0) {
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: FormData
+            }); 
+            if (response.ok) {
+                form.classList.add('_sending')
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+            } else {
+                alert('Ошибка')
+            }
+        } else {
+            alert('Заполните обязательные поля')
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++){
+            const input = formReq[index];
+            formRemoveError(input)
+
+            if (input.classList.contains('_email')){
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === ''){
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error
+    }
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
+// #    #     #     #    F  O  R  M     #    #     #     #
 
 
